7a974c70b05eb2e4632fe6ac397f56dba50e2cd9
chipoglesby <chip@creativesparkcolumbia.com>
2018-04-12 11:13:10 -0500

    Removing file

--- code/srtImport.R
+++
@@ -1,9 +0,0 @@
-library(subtools)
-library(magrittr)
-
-a <- read.subtitles.serie(dir = "subtitles/series/")
-subtitles <- subDataFrame(a) 
-
-subtitles %<>%
-  select(-season_num, -serie) %>% 
-  mutate(season = as.integer(season))
