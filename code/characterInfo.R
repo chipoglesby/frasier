acf04a19cadc54d823733b43c5a3e0b5ad6bb941
chipoglesby <chip@creativesparkcolumbia.com>
2018-04-14 23:08:11 -0500

    Updating info.

--- code/characterInfo.R
+++ code/characterInfo.R
@@ -1,9 +1,8 @@
-library('rvest')
 
 url <- 'http://www.imdb.com/title/tt0106004/fullcredits/?ref_=tt_ov_st_sm'
 
 mainCharacter <- "^frasier|^daphne moon$|^roz doyle$|^niles crane$|^martin crane$|^bulldog briscoe$|^eddie$"
-reccurringCharacter <- "bebe|^gil|noel shempsky|donny douglas|^kenny$|gertrude|simon|kirby|alice may doyle"
+reccurringCharacter <- "bebe|^gil|noel shempsky|donny douglas|^kenny daly$|gertrude|simon|^kirby$"
 
 url %>% 
   read_html() %>% 
@@ -17,12 +16,12 @@ url %>%
   mutate(characterName = trimws(
     gsub('/ ...|#|Dr.|\\n', '',
          sapply(strsplit(characterName, '\\d',"-"), `[`, 1))),
-    mainCharacter = as.factor(
-      grepl(mainCharacter, tolower(characterName))),
-    recurringCharacter = as.factor(
-      grepl(reccurringCharacter, tolower(characterName))),
+    characterType = as.factor(ifelse(
+      grepl(mainCharacter, tolower(characterName)), 
+      "main",
+      "recurring")),
     firstName = sapply(strsplit(characterName, ' '), `[`, 1)) -> characterInfo
 
 characterInfo %>% 
-filter(mainCharacter == TRUE | recurringCharacter == TRUE,
-       !grepl('hope|ashley', tolower(actorName))) -> mainCharacters
+  filter(characterType == 'main'|
+           grepl(reccurringCharacter, tolower(characterName))) -> mainCharacters
