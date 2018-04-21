ecf835c60cab2e46bf188a0ca95dd27f2ddb35cf
chipoglesby <chip@creativesparkcolumbia.com>
2018-04-21 16:10:33 -0400

    Updating analysis code

--- code/analysis/transcriptAnalysis.R
+++ code/analysis/transcriptAnalysis.R
@@ -4,61 +4,61 @@
 lines %>%
   filter(grepl('san gennaro|bavetta|au pied de cochon|espalier|volant|chez chez|cochon noir|heureux|chez henri|Anya|happy brothers|chicken chicken', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> favoriteResturants
 
 # Who talks about Sherry?
 lines %>%
   filter(grepl('sherry', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> sherryReferences
 
 # Who talks about Coffee?
 lines %>%
   filter(grepl('coffee|café|latte|nutmeg|nervosa', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> coffeReferences
 
 # Who talks about Eddie??
 lines %>%
   filter(grepl('eddie', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> edditReferences
 
 # Who talks about beer?
 lines %>%
   filter(grepl('beer', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> beerReferences
 
 # Who talks about Opera?
 lines %>%
   filter(grepl('opera|theatre|theater', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> operaReferences
 
 # Who talks about Roz?
 lines %>%
   filter(grepl('roz', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> rozReferences
 
 # Who talks about Daphne?
 lines %>%
   filter(grepl('daphne', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> daphneReferences
 
 # Who talks about Maris?
 lines %>%
   filter(grepl('maris', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> marisReferences
 
 # Who talks about Lilith?
 lines %>%
   filter(grepl('lilith', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> lilithReferences
 
 # Which brother introduces themselves as doctor?
 lines %>%
@@ -66,7 +66,7 @@ lines %>%
                tolower(lines)),
          grepl('frasier|niles',
                tolower(character))) %>%
-  count(character, sort = TRUE)
+  count(character, sort = TRUE) -> drReferences
 
 # Who talks about mom the most?
 # Which brother introduces themselves as doctor?
@@ -75,7 +75,7 @@ lines %>%
                tolower(lines)),
          grepl('frasier|niles|martin',
                tolower(character))) %>%
-  count(character, sort = TRUE)
+  count(character, sort = TRUE) -> momReferences
 
 # How many times does frasier say hello seattle or I'm listening?
 lines %>%
@@ -83,37 +83,23 @@ lines %>%
                tolower(lines)),
          grepl('frasier',
                tolower(character))) %>%
-  count(character, sort = TRUE)
+  count(character, sort = TRUE) -> listeningReferences
 
 # Is Frasier the only one who says DEAR GOD
 lines %>%
   filter(grepl('dear god', tolower(lines))) %>%
   count(character, sort = TRUE) %>%
-  top_n(5, n)
+  top_n(5, n) -> godReferences
 
 lines %>%
-  # filter(grepl('roz', tolower(character))) %>%
-  filter(!is.na(lines)) %>%
-  # group_by(character) %>%
+  filter(!is.na(lines),
+         grepl('frasier|roz|daphne|martin|niles|bulldog', 
+               tolower(character))) %>%
   unnest_tokens(word, lines) %>%
   anti_join(stop_words) %>%
-  count(word, sort = TRUE) %>%
+  count(character, word, sort = TRUE) %>%
+  arrange(desc(n)) %>% 
   filter(n > 500) %>%
-  mutate(word = reorder(word, n)) %>%
-  ggplot(aes(word, n)) +
-  geom_col() +
-  xlab(NULL) +
-  coord_flip()
-
-
-lines %>% 
-  filter(grepl('good lord|dear god|dear lord', lines)) %>% 
-  tally()
-
-lines %>% 
-  filter(grepl('gods sake', lines)) %>% 
-  tally()
-
-lines %>% 
-  filter(grepl('good lord', lines)) %>% 
-  tally()
+  ggplot(aes(n, word)) +
+  geom_barh(stat = 'identity') +
+  facet_wrap(~character, scales = 'free')
