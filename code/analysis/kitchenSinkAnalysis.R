2c38e2e99143094b2d59084dd57306c9e4a4a34e
chipoglesby <chip@creativesparkcolumbia.com>
2018-05-08 02:07:25 -0500

    Updating analysis

--- code/analysis/kitchenSinkAnalysis.R
+++ code/analysis/kitchenSinkAnalysis.R
@@ -104,3 +104,110 @@ transcripts %>%
   ggplot(aes(n, word)) +
   geom_barh(stat = 'identity') +
   facet_wrap(~character, scales = 'free')
+
+# New
+transcripts %>% 
+  filter(characterType == 'main') %>% 
+  count(season, character) %>% 
+  ggplot(aes(character, n, fill = character)) +
+  geom_col() +
+  facet_wrap(~season)
+
+tidyTranscripts %>% 
+  filter(grepl('^(martin|frasier|fras|daphne|roz|dad|niles|son|marty|daph)$', word),
+         characterType == 'main') %>% 
+  count(character, word) %>%
+  ggplot(aes(word, n, fill = word)) +
+  geom_col() +
+  facet_wrap(~character,
+             scales = 'free')
+
+
+tidyTranscripts %>% 
+  filter(grepl('^(she|he|i|they|we|me|you|her|him|guy|girl|man|woman)$', word),
+         characterType == 'main') %>% 
+  count(character, word) %>%
+  ggplot(aes(word, n, fill = character)) +
+  geom_col() +
+  facet_wrap(~character,
+             scales = 'free')
+
+tidyTranscripts %>% 
+  filter(grepl('^(she|he|him|her|his|guy|girl|man|woman)$', word),
+         characterType == 'main') %>% 
+  mutate(type = if_else(grepl('^(he|him|his|guy|man)$', word), 'male', 'female')) %>% 
+  count(character, word, type) %>%
+  ggplot(aes(type, n, fill = type)) +
+  geom_col() +
+  facet_wrap(~character)
+
+tidyTranscripts %>% 
+  filter(grepl('^(me|i|you|they|we)$', word),
+         characterType == 'main') %>% 
+  mutate(type = if_else(grepl('me|i|we', word), "self", "others")) %>% 
+  count(character, word, type) %>%
+  ggplot(aes(type, n, fill = type)) +
+  geom_col() +
+  facet_wrap(~character)
+
+
+
+transcripts %>%
+  unnest_tokens(words, lines, token = "ngrams", n = 3) %>% 
+  filter(characterType == 'main') %>% 
+  count(character, words, sort = TRUE) -> trigrams
+
+transcripts %>%
+  unnest_tokens(words, lines, token = "ngrams", n = 2) %>% 
+  filter(characterType == 'main') %>% 
+  count(character, words, sort = TRUE) -> bigrams
+
+transcripts %>%
+  unnest_tokens(word, lines) %>% 
+  filter(characterType == 'main') %>% 
+  anti_join(stop_words) %>% 
+  count(character, word, sort = TRUE) -> unigrams
+
+transcripts %>%
+  filter(character == 'Daphne',
+         grepl('crane|niles|frasier', tolower(lines))) %>%
+  mutate(sType = as.factor(if_else(grepl('crane', tolower(lines)), 'formal', 'informal'))) %>% 
+  count(season,
+        sType) %>% 
+  ggplot(aes(season, n, color = sType)) +
+  geom_line(size = .75) +
+  scale_x_continuous(breaks = c(1:max(transcripts$season)))
+
+
+
+transcripts %>%
+  filter(character == 'Martin',
+         grepl('fras|niles|frasier', tolower(lines))) %>%
+  mutate(sType = as.factor(if_else(grepl('fras|frasier', tolower(lines)), 'frasier', 'niles'))) %>% 
+  count(season,
+        sType) %>% 
+  ggplot(aes(season, n, color = sType)) +
+  geom_line(size = .75) +
+  scale_x_continuous(breaks = c(1:max(transcripts$season)))
+
+
+transcripts %>%
+  filter(character == 'Niles',
+         grepl('maris|daphne', tolower(lines))) %>%
+  mutate(sType = as.factor(if_else(grepl('maris', tolower(lines)), 'maris', 'daphne'))) %>% 
+  count(season,
+        sType) %>% 
+  ggplot(aes(season, n, color = sType)) +
+  geom_line(size = .75) +
+  scale_x_continuous(breaks = c(1:max(transcripts$season)))
+
+
+transcripts %>%
+  filter(character == 'Roz',
+         grepl('love|sleep|slept|sex', tolower(lines))) %>%
+  mutate(sType = as.factor(if_else(grepl('sle|sex', tolower(lines)), 'slept together', 'love'))) %>% 
+  count(season,
+        sType) %>% 
+  ggplot(aes(season, n, color = sType)) +
+  geom_line(size = .75) +
+  scale_x_continuous(breaks = c(1:max(transcripts$season)))
